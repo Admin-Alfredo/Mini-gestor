56bea13314c6739dcf0e88d5bbdb9bbd87b840eb
Admiro André <admiroalfredo1742@gmail.com>
2023-08-20 22:39:17 +0100

    start development!

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,14 @@
+import Header from "./components/Header"
+import { AppContainer } from "./styles"
 
 function App() {
 
   return (
-    <>
+    <AppContainer>
+      <Header></Header>
      My App Mini-Gestor!
      <a href="#">click aqui</a>
-    </>
+    </AppContainer>
   )
 }
 
---
+++ app/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+interface IProps {
+
+}
+function Header(props: IProps) {
+	return (
+		<Container>
+			
+		</Container>
+	)
+}
+export default Header;
